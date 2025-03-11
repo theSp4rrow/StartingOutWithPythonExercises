57522e7fad90ec9311a2c3b13f8d9e4b36292efb
theSp4rrow <ali.alasfour@siu.edu>
2025-03-11 01:30:05 -0500

    first two

---
+++  chapter2/SalesPrediciton.py
@@ -0,0 +1,4 @@
+#SalesPrediction
+ANNUALPROFIT = 0.25
+sales =int(input("Please enter the projected amount of sales: "))
+print("Projected profit: ",sales*ANNUALPROFIT,"." ,sep='')
---
+++  chapter2/personalInformation.py
@@ -0,0 +1,18 @@
+#personal Information
+print("Please enter your personal information.")
+name = input("Name:")
+major = input("Major:")
+city = input("City:")
+state = input("State intials:")
+zip = input("Zipcode:")
+number = input("Phone number:")
+print("__________OUTPUT___________")
+print("NAME:",name)
+print("MAJOR:",major)
+print("LOCATION:",city,state,zip)
+print("PHONE NUMBER:",number)
+#example output
+# Cameron Calero
+# 2045 Heliport Loop
+# Riverfolk, IN 43286
+# 812-555-1212
